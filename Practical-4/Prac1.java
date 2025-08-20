9879ca027a5e6a9e25d88b40917e4a0b453a9593
IamVatsal <vatsalpatel0609@gmail.com>
2025-08-20 21:35:01 +0530

    add prac1 in Practical-4

---
+++ Practical-4/Prac1.java
@@ -0,0 +1,31 @@
+public class Prac1 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Vatsal", "24012011142", "Computer Engineering");
+        st1.display();
+    }
+}
+
+public class Student{
+    String name;
+    String id;
+    String course;
+
+    public Student(String name, String id, String course) {
+        this.name = name;
+        this.id = id;
+        this.course = course;
+    }
+
+    public void setDetails(String name,String id,String course){
+        this.name = name;
+        this.id = id;
+        this.course = course;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Course: " + course);
+    }
+    
+}
