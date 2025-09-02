b3bccc556e71b28f4e6162959dd26fd5060ecbde
Vatsal Patel <vatsalpatel0609@gmail.com>
2025-09-03 01:03:24 +0530

    update prac8 in 2 and prac1 in 4

--- Practical-2/Prac11.java
+++ Practical-2/Prac11.java
@@ -16,3 +16,52 @@ public class Prac11 {
         }
     }
 }
+
+public class Prac11While {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        System.out.print("Prime Numbers (while): ");
+
+        int i = 1;
+        while (i <= num) {
+            boolean isPrime = true;
+            int j = 2;
+            while (j <= Math.sqrt(i)) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+                j++;
+            }
+            if (isPrime && i > 1) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+    }
+}
+
+public class Prac11DoWhile {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        System.out.print("Prime Numbers (do-while): ");
+
+        int i = 1;
+        do {
+            boolean isPrime = true;
+            int j = 2;
+            do {
+                if (j <= Math.sqrt(i) && i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+                j++;
+            } while (j <= Math.sqrt(i));
+
+            if (isPrime && i > 1) {
+                System.out.print(i + " ");
+            }
+            i++;
+        } while (i <= num);
+    }
+}
